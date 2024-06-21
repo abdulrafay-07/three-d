029256ed8f5bd583185f85494dfb7ea268427b97
Shayan hanif <shayanhanif50@gmail.com>
2024-06-21 12:40:45 +0500

    text animation

--- components/homepage/Text.jsx
+++ components/homepage/Text.jsx
@@ -5,16 +5,18 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 const Text = () => {
     useGSAP(() => {
-
-        gsap.fromTo('#text', {
-            y: 10, duration: 1, stagger:0.3, opacity:0
-        },{
-            opacity:1, y:0, duration:1
+        // const tl = gsap.timeline();
+        // gsap.to
+        gsap.from(['#text', "#text2"], {
+            y: 10, duration: 1, delay:0.3, stagger:0.3, opacity:0
         })
+        // },{
+        //     opacity:1, y:0, duration:1
+        // })
     }, [])
     return (
         <div>
-            <p id='text' className='text-gray-400'>Welcome to blue Yard</p>
+            <p id='text' className='text-gray-400 text-center'>THIS WEBSITE USES AUDIO TO ENHANCE <br /> <span id='text2'> YOUR EXPERIENCE</span></p>
         </div>
     )
 }
